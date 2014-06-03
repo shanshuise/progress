9d25e504f8e8b91d0a4af51c8c9da9e4b06f8760
shanshuise <shanshuise@gmail.com>
2014-06-03 17:30:32 +0800

    modify models.py

--- progress/admin.py
+++ progress/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from progress.models import Progress, User
 
-# Register your models here.
+admin.site.register([Progress, User])
--- progress/models.py
+++ progress/models.py
@@ -1,9 +1,32 @@
+#coding:utf-8
 from django.db import models
 
 class Progress(models.Model):
     PROGRESS_CHOICES = (
-        (u'1', u')
+        (u'1', u'正在编制')
+        (u'2', u'初稿已完成')
+        (u'3', u'正在审后修改')
+        (u'2', u'终稿已完成')
     )
-    username = models.ForeignKey(User)
+    puser = models.ForeignKey(User)
     pname = models.CharField(max_length=50)
     pprogress = models.CharField(max_length=50, choices=PROGRESS_CHOICES)
+    remark = models.CharField(max_length=100)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.puser
+
+class User(models.Model):
+    DEPARTMENT_CHOICES = (
+        (u'1', u'一所')
+        (u'2', u'二所')
+        (u'3', u'三所')
+    )
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=20)
+    deparment = models.CharField(max_length=10, choices=DEPARTMENT_CHOICES)
+
+    def __unicode__(self):
+        return self.username
